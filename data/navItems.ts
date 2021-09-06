1ee49a1ec6e47a43cf45726a6f47f69d88933d7c
Boris Pöhland <boris.poehland@gmail.com>
2021-09-06 14:17:51 +0200

    changed links

--- data/navItems.ts
+++ data/navItems.ts
@@ -6,12 +6,12 @@ const getNavItems = (): INavItem[] => {
   return [
     {
       name: 'Linked article',
-      href: 'https://www.borispoehland.com',
+      href: 'https://www.borispoehland.com/blog/Awesome%20landing%20page%20with%20Next.js%20and%20Scrollmagic',
       icon: BiBookBookmark,
     },
     {
       name: 'Source code',
-      href: 'https://www.github.com',
+      href: 'https://github.com/borispoehland/blog_awesome_landing_page',
       icon: AiOutlineGithub,
     },
   ];
